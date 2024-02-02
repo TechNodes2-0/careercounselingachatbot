cbdc529cd4e97ea913afde683e260fc10643a2bd
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-02 13:44:18 +0530

    Refactor webhook route and add AI chatbot page

--- careercounselingbot/app/page.jsx
+++ careercounselingbot/app/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { connectToDatabase } from "../lib/mongoose";
 function page() {
+  connectToDatabase();
+
   return (
     <div className="flex items-center justify-center">
-      <h2 className="text-sky-500 font-bold"> AI chatbot</h2>
+      <h2 className="font-bold text-sky-500"> AI chatbot</h2>
     </div>
   );
 }
