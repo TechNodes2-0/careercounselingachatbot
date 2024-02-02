373a486d8101c9e1119e1a77bb0afbab7c0bd52c
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-02 11:37:18 +0530

    Refactor code for improved performance

---
+++ careercounselingbot/app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex items-center justify-center">
+      <h2 className="text-sky-500 font-bold"> BoilerPLate Code LOL </h2>
+    </div>
+  );
+}
+
+export default page;
