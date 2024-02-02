2e97a44bc21136893d54a28e9c446cffb07b0ad3
JayeshYadav99 <jayeshyadav528@gmail.com>
2024-02-02 12:20:51 +0530

    Added Clerk Basic authentication

--- careercounselingbot/app/page.jsx
+++ careercounselingbot/app/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function page() {
   return (
     <div className="flex items-center justify-center">
-      <h2 className="text-sky-500 font-bold"> BoilerPLate Code LOL </h2>
+      <h2 className="text-sky-500 font-bold"> AI chatbot</h2>
     </div>
   );
 }
