7e72a6c55dfd1baf3b9a2681c0b65883e6f9b1e2
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-03 22:33:18 +0530

    Add message condition to display suggestions only when message length is 0

--- careercounselingbot/app/(root)/chat/page.jsx
+++ careercounselingbot/app/(root)/chat/page.jsx
@@ -142,82 +142,6 @@ function page() {
                       sender={message.sender}
                     />
                   ))}
-                  {/* <div className="p-3 rounded-lg col-start-61col-end-13">
-                    <div className="flex flex-row-reverse items-center justify-start">
-                      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-indigo-200 rounded-full">
-                      <Image 
-                src={'https://cdn-icons-png.flaticon.com/512/6873/6873405.png' }
-                alt="Picture of the author"
-                width={50}
-                height={50}
-                />
-                      </div>
-                      <div className="relative px-4 py-2 mr-3 text-sm bg-indigo-100 shadow rounded-xl">
-                        <div>Are you exploring career options, seeking advice on job applications, or something else?</div>
-                      </div>
-                    </div>
-                  </div> */}
-
-                  {/* <div className="col-start-1 col-end-8 p-3 rounded-lg">
-
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-indigo-200 rounded-full">
-                      User
-                      </div>
-                      <div className="relative px-4 py-2 ml-3 text-sm bg-white shadow rounded-xl">
-                        <div>Hi there.</div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div className="flex flex-row-reverse items-center justify-start">
-                      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-indigo-200 rounded-full">
-                        A
-                      </div>
-                      <div className="relative px-4 py-2 mr-3 text-sm bg-indigo-100 shadow rounded-xl">
-                        <div>I'm ok what about you?</div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-indigo-200 rounded-full">
-                        A
-                      </div>
-                      <div className="relative px-4 py-2 ml-3 text-sm bg-white shadow rounded-xl">
-                        <div>Lorem ipsum dolor sit amet !</div>
-                      </div>
-                    </div>
-                  </div> */}
-
-                  {/* <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div className="flex flex-row-reverse items-center justify-start">
-                      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-indigo-200 rounded-full">
-                        A
-                      </div>
-                      <div className="relative px-4 py-2 mr-3 text-sm bg-indigo-100 shadow rounded-xl">
-                        <div>
-                          Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-indigo-200 rounded-full">
-                        A
-                      </div>
-                      <div className="relative px-4 py-2 ml-3 text-sm bg-white shadow rounded-xl">
-                        <div>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Perspiciatis, in.
-                        </div>
-                      </div>
-                    </div>
-                  </div> */}
                 </div>
               </div>
             </div>
@@ -225,106 +149,24 @@ function page() {
             {/* 
           input field */}
             {isOpen && (
-              <div className="max-h-56 rounded-xl bg-white w-full px-4 py-2 my-4">
-                <div className="hover:bg-gray-100 w-full my-2 py-2 px-2 rounded-lg">
-                  <p className=" text-md text-black font-semibold">/gif</p>
-                  <p className=" text-sm text-gray-700">search animated gif</p>
+              <div className="w-full px-4 py-2 my-4 bg-white max-h-56 rounded-xl">
+                <div className="w-full px-2 py-2 my-2 rounded-lg hover:bg-gray-100">
+                  <p className="font-semibold text-black text-md">/gif</p>
+                  <p className="text-sm text-gray-700 ">search animated gif</p>
                 </div>
-                <div className="hover:bg-gray-100 w-full my-2 py-2 px-2 rounded-lg">
-                  <p className=" text-md text-black font-semibold">/gif</p>
-                  <p className=" text-sm text-gray-700">search animated gif</p>
+                <div className="w-full px-2 py-2 my-2 rounded-lg hover:bg-gray-100">
+                  <p className="font-semibold text-black text-md">/gif</p>
+                  <p className="text-sm text-gray-700 ">search animated gif</p>
                 </div>
               </div>
             )}
             <div className="">
-              <PromteSuggestion />
+              <PromteSuggestion message={messages} />
             </div>
             <UserInput
               onMessageSubmit={handleUserMessage}
               onInputChange={inputChange}
             />
-            {/* <div className="flex flex-row items-center w-full h-16 px-4 bg-white rounded-xl">
-            {isOpen && (
-              <div className="max-h-56 rounded-xl bg-white w-full px-4 py-2 my-4">
-                <div className="hover:bg-gray-100 w-full my-2 py-2 px-2 rounded-lg">
-                  <p className=" text-md text-black font-semibold">/gif</p>
-                  <p className=" text-sm text-gray-700">search animated gif</p>
-                </div>
-                <div className="hover:bg-gray-100 w-full my-2 py-2 px-2 rounded-lg">
-                  <p className=" text-md text-black font-semibold">/gif</p>
-                  <p className=" text-sm text-gray-700">search animated gif</p>
-                </div>
-              </div>
-            )}
-            <UserInput
-              onMessageSubmit={handleUserMessage}
-              onInputChange={inputChange}
-            />
-            {/* <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
-              <div>
-                <button className="flex items-center justify-center text-gray-400 hover:text-gray-600">
-                  <svg
-                    className="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-              <div className="flex-grow ml-4">
-                <div className="relative w-full">
-                  <input
-                    type="text"
-                    className="flex w-full h-10 pl-4 border rounded-xl focus:outline-none focus:border-indigo-300"
-                  />
-                  <button className="absolute top-0 right-0 flex items-center justify-center w-12 h-full text-gray-400 hover:text-gray-600">
-                    <svg
-                      className="w-6 h-6"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-              <div className="ml-4">
-                <button className="flex items-center justify-center flex-shrink-0 px-4 py-1 text-white bg-indigo-200 hover:bg-indigo-600 rounded-xl">
-                  <span>Send</span>
-                  <span className="ml-2">
-                    <svg
-                      className="w-4 h-4 -mt-px transform rotate-45"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                      ></path>
-                    </svg>
-                  </span>
-                </button>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
